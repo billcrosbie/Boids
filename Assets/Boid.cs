12c927743d913c19c3099ed6422d2ab9992b2067
Bill Crosbie <G00175895@RVCC-10878P1.raritanval.edu>
2015-06-11 17:05:04 -0400

    Working Boids from class

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -19,7 +19,7 @@ public class Boid : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		if (boids == null) {
-			boids = new List<><Boid>();
+			boids = new List<Boid>();
 		}
 
 		boids.Add (this);
@@ -39,7 +39,7 @@ public class Boid : MonoBehaviour {
 		// make this.transform a child of the Boids GO
 		this.transform.parent = GameObject.Find ("Boids").transform;
 
-		Color rnadColor = Color.black;
+		Color randColor = Color.black;
 		while (randColor.r + randColor.g + randColor.b < 1.0f) {
 			randColor = new Color(Random.value, Random.value,Random.value);
 		}
@@ -52,8 +52,103 @@ public class Boid : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		List<Boid> neighbors = GetNeighbors (this);
+		
+		newVelocity = velocity;
+		newPosition = this.transform.position;
+		
+		Vector3 neighborVel = GetAverageVelocity (neighbors);
+		newVelocity += neighborVel * BoidSpawner.S.velocityMatchingAmt;
+		
+		Vector3 neighborCenterOffset = GetAveragePosition(neighbors) - this.transform.position;
+		newVelocity += neighborCenterOffset*BoidSpawner.S.flockCenteringAmt;
+		
+		Vector3 dist;
+		if (collisionRisks.Count > 0) {
+			Vector3 collisionAveragePos=GetAveragePosition(collisionRisks);
+			dist = collisionAveragePos - this.transform.position;
+			newVelocity += dist * BoidSpawner.S.collisionAvoidanceAmt;
+		}
+		
+		dist = BoidSpawner.S.mousePos - this.transform.position;
+		if (dist.magnitude > BoidSpawner.S.mouseAvoidanceDist) {
+			newVelocity += dist * BoidSpawner.S.mouseAttractionAmt;
+		} else {
+			newVelocity -= dist.normalized * BoidSpawner.S.mouseAvoidanceDist * BoidSpawner.S.mouseAvoidanceAmt;
+		}
+		
+	}
 	
-
-
+	void LateUpdate() {
+		velocity = (1 - BoidSpawner.S.velocityLerpAmt) * velocity + BoidSpawner.S.velocityLerpAmt * newVelocity;
+		
+		
+		if (velocity.magnitude > BoidSpawner.S.maxVelocity) {
+			velocity = velocity.normalized * BoidSpawner.S.maxVelocity;
+		}
+		
+		if (velocity.magnitude < BoidSpawner.S.maxVelocity) {
+			velocity = velocity.normalized * BoidSpawner.S.minVelocity;
+		}
+		
+		newPosition = this.transform.position + velocity * Time.deltaTime;
+		newPosition.y = 0;
+		this.transform.LookAt (newPosition);
+		this.transform.position = newPosition;
+		
+	}
+	
+	public List <Boid> GetNeighbors (Boid boi) {
+		float closestDist = float.MaxValue;
+		Vector3 delta;
+		float dist;
+		neighbors.Clear ();
+		collisionRisks.Clear ();
+		
+		foreach (Boid b in boids) {
+			
+			if (b == boi)
+				continue;
+			delta = b.transform.position - boi.transform.position;
+			dist = delta.magnitude;
+			
+			if (dist < closestDist) {
+				closestDist = dist;
+				closest = b;
+			}
+			
+			if (dist < BoidSpawner.S.nearDist) {
+				neighbors.Add (b);
+			}
+			
+			if (dist < BoidSpawner.S.collisionDist) {
+				collisionRisks.Add (b);
+			}
+		}
+		
+		if (neighbors.Count == 0) {
+			neighbors.Add (closest);
+		}
+		
+		return(neighbors);
+		
+	}
+	
+	public Vector3 GetAveragePosition (List<Boid> someBoids) {
+		Vector3 sum = Vector3.zero;
+		foreach (Boid b in someBoids) {
+			sum += b.transform.position;
+		}
+		Vector3 center = sum / someBoids.Count;
+		return (center);
+	}
+	
+	public Vector3 GetAverageVelocity (List<Boid> someBoids) {
+		Vector3 sum = Vector3.zero;
+		foreach (Boid b in someBoids) {
+			sum += b.velocity;
+		}
+		Vector3 avg = sum / someBoids.Count;
+		return (avg);
 	}
 }
